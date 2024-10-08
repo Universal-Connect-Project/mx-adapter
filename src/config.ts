91cc2d15aeb7b92d3a53b1df9bf2f8752491bcd4
Tyson Phalp <tyson.phalp@mx.com>
2024-10-08 10:10:35 -0600

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+import * as dotenv from "dotenv";
+
+export const init = (path = ".env") => {
+  const result: dotenv.DotenvConfigOutput = dotenv.config({
+    path
+  });
+
+  if (result.error) {
+    throw result.error;
+  }
+
+  const { parsed: envs } = result;
+
+  // Validate envs
+  if (!envs?.HostUrl) {
+    throw new Error("Missing HostUrl. Check README.md and `../.env.example` for more info.");
+  }
+
+  if (!envs?.MxClientId && !envs?.MxClientIdProd) {
+    throw new Error("Missing MxClientId or MxClientIdProd. Check README.md and `../.env.example` for more info.");
+  }
+
+  if (!envs?.MxApiSecret && !envs?.MxApiSecretProd) {
+    throw new Error("Missing MxApiSecret or MxApiSecretProd. Check README.md and `../.env.example` for more info.");
+  }
+
+  return {
+    HostUrl: envs?.HostUrl,
+    MxClientId: envs?.MxClientId,
+    MxApiSecret: envs?.MxApiSecret,
+    MxClientIdProd: envs?.MxClientIdProd,
+    MxApiSecretProd: envs?.MxApiSecretProd
+  };
+};
+
+export default init();
---
+++ src/createVc.test.ts
@@ -0,0 +1,61 @@
+import { VCDataTypes } from './contract'
+
+import {
+  mxVcAccountsData,
+  mxVcIdentityData,
+  mxVcIntegrationAccountsData,
+  mxVcTranscationsData
+} from './test/testData/mxVcData'
+import { mxIntGetVC, mxProdGetVC } from './createVc'
+import { logClient } from "./__mocks__/logClient";
+import { aggregatorCredentials } from "./adapter.test";
+
+
+const dependencies = {
+  logClient,
+  aggregatorCredentials
+};
+
+
+describe('getVc', () => {
+  const connectionId = 'connectionId'
+  const userId = 'userId'
+  const accountId = 'accountId'
+
+  it('gets accounts VC from INT environment', async () => {
+    const vc = await mxIntGetVC(dependencies)({
+      connectionId,
+      type: VCDataTypes.ACCOUNTS,
+      userId
+    })
+    expect(vc).toEqual(mxVcIntegrationAccountsData)
+  })
+
+  it('gets identity VC from Prod environment', async () => {
+    const vc = await mxProdGetVC(dependencies)({
+      connectionId,
+      type: VCDataTypes.IDENTITY,
+      userId
+    })
+    expect(vc).toEqual(mxVcIdentityData)
+  })
+
+  it('gets accounts VC from Prod environment', async () => {
+    const vc = await mxProdGetVC(dependencies)({
+      connectionId,
+      type: VCDataTypes.ACCOUNTS,
+      userId
+    })
+    expect(vc).toEqual(mxVcAccountsData)
+  })
+
+  it('gets transactions VC from Prod environment', async () => {
+    const vc = await mxProdGetVC(dependencies)({
+      connectionId,
+      type: VCDataTypes.TRANSACTIONS,
+      userId,
+      accountId
+    })
+    expect(vc).toEqual(mxVcTranscationsData)
+  })
+})
